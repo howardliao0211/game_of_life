44b7c724a37e943b1e7107730cd40765d7e53cb8
howardliao0211 <howard.liao.hh@gmail.com>
2023-06-24 16:16:42 +0800

    Add conway's game of life rules

--- cell.py
+++ cell.py
@@ -4,27 +4,26 @@ import pygame
 class CELL():
 
     def __init__(self, screen, x, y, cell_state: CellState):
-        self.screen = screen
         self.x = x
         self.y = y
         self.cell_state = cell_state
 
         if cell_state is CellState.ALIVE:
-            self.set_to_alive()
+            self.set_to_alive(screen)
         elif cell_state is CellState.DEAD:
-            self.set_to_dead()
+            self.set_to_dead(screen)
 
-    def set_to_dead(self):
+    def set_to_dead(self, screen):
         self.cell_state = CellState.DEAD
-        pygame.draw.rect(self.screen, BLACK, pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE))
-        pygame.draw.rect(self.screen, WHITE, pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE), 1)
+        pygame.draw.rect(screen, BLACK, pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE), 1)
 
-    def set_to_alive(self):
+    def set_to_alive(self, screen):
         self.cell_state = CellState.ALIVE
-        pygame.draw.rect(self.screen, CYAN, pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(screen, CYAN, pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE))
 
-    def change_state(self):
+    def change_state(self, screen):
         if self.cell_state is CellState.DEAD:
-            self.set_to_alive()
+            self.set_to_alive(screen)
         elif self.cell_state is CellState.ALIVE:
-            self.set_to_dead()
+            self.set_to_dead(screen)
--- generation.py
+++ generation.py
@@ -1,49 +1,90 @@
 from constants import *
 from cell import CELL
 import pygame
+import sys
 import copy
-
 
 class GENERATION:
     def __init__(self, screen):
+        self.screen = screen
         self.started = False
         self.cell_list = []
-        self.draw_grid(screen)
+        self.draw_grid()
 
-    def draw_grid(self, screen):
+    def draw_grid(self):
         BLOCK_SIZE  # Set the size of the grid block
         for x in range(0, GAME_WIDTH, BLOCK_SIZE):
             temp_cell_list = []
             for y in range(0, GAME_HEIGHT, BLOCK_SIZE):
-                cell = CELL(screen, x, y, CellState.DEAD)
+                cell = CELL(self.screen, x, y, CellState.DEAD)
                 temp_cell_list.append(cell)
             self.cell_list.insert(x, temp_cell_list)
 
     def set_to_dead(self, x, y):
-        self.cell_list[x][y].set_to_dead()
+        self.cell_list[x][y].set_to_dead(self.screen)
 
     def set_to_alive(self, x, y):
-        self.cell_list[x][y].set_to_alive()
+        self.cell_list[x][y].set_to_alive(self.screen)
 
     def change_state(self, x, y):
-        self.cell_list[x][y].change_state()
+        self.cell_list[x][y].change_state(self.screen)
 
     def start(self):
         self.started = True
 
     def update(self):
-        cell_list_deep_copy = copy.deepcopy(self.cell_list)
+
+        if not self.started:
+            return
+
+        live_neighnor_num = 0
+        cell_list_dc = copy.deepcopy(self.cell_list)
+
+        for x in range(CELL_WIDTH):
+            for y in range(CELL_HEIGHT):
+                live_neighnor_num = self.__find_live_neighbor_num(cell_list_dc, x, y)
+                if(self.cell_list[x][y].cell_state is CellState.ALIVE):
+                    if(live_neighnor_num < 2):
+                        self.set_to_dead(x, y)
+                    elif(live_neighnor_num == 2 or live_neighnor_num == 3):
+                        self.set_to_alive(x, y)
+                    elif(live_neighnor_num > 3):
+                        self.set_to_dead(x, y)
+                else:
+                    if(live_neighnor_num == 3):
+                        self.set_to_alive(x, y)
 
     def __find_live_neighbor_num(self, cell_list, x, y):
         live_neighbor = 0
         neighbor_dict = {
                          'top_left':    [x - 1, y - 1],
-                         'top' :        [x, y - 1],
-                         'top_right' :  [x + 1, y + 1],
+                         'top' :        [x,     y - 1],
+                         'top_right' :  [x + 1, y - 1],
                          'left' :       [x - 1, y],
                          'right' :      [x + 1, y],
                          'bot_left' :   [x - 1, y + 1],
-                         'bot' :        [x, y + 1],
+                         'bot' :        [x,     y + 1],
                          'bot_right' :  [x + 1, y + 1],
                         }
-        pass
+        if x == 0:
+            neighbor_dict['top_left'][0] = MAX_CELL_X
+            neighbor_dict['left'][0] = MAX_CELL_X
+            neighbor_dict['bot_left'][0] = MAX_CELL_X
+        elif x == MAX_CELL_X:
+            neighbor_dict['top_right'][0] = 0
+            neighbor_dict['right'][0] = 0
+            neighbor_dict['bot_right'][0] = 0
+        if y == 0:
+            neighbor_dict['top_left'][1] = MAX_CELL_Y
+            neighbor_dict['top'][1] = MAX_CELL_Y
+            neighbor_dict['top_right'][1] = MAX_CELL_Y
+        elif y == MAX_CELL_Y:
+            neighbor_dict['bot_left'][1] = 0
+            neighbor_dict['bot'][1] = 0
+            neighbor_dict['bot_right'][1] = 0
+
+        for coord in neighbor_dict.values():
+            if cell_list[coord[0]][coord[1]].cell_state == CellState.ALIVE:
+                live_neighbor += 1
+
+        return live_neighbor
